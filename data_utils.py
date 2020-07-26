07f77851512c98ca09d4f8add68f4be4eaaf4cdd
msalari <msalari@sfu.ca>
2020-07-26 13:41:56 -0700

    Add ImageFolder for multi-view dataset

---
+++ data_utils.py
@@ -0,0 +1,134 @@
+import torch
+from torchvision.datasets.vision import VisionDataset
+
+from PIL import Image
+
+import os
+import os.path
+
+
+def has_file_allowed_extension(filename, extensions):
+    return filename.lower().endswith(extensions)
+
+
+def is_image_file(filename):
+    return has_file_allowed_extension(filename, IMG_EXTENSIONS)
+
+
+def make_dataset(directory, class_to_idx, extensions=None, is_valid_file=None):
+    instances = []
+    directory = os.path.expanduser(directory)
+    both_none = extensions is None and is_valid_file is None
+    both_something = extensions is not None and is_valid_file is not None
+    if both_none or both_something:
+        raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+    if extensions is not None:
+        def is_valid_file(x):
+            return has_file_allowed_extension(x, extensions)
+    for target_class in sorted(class_to_idx.keys()):
+        class_index = class_to_idx[target_class]
+        target_dir = os.path.join(directory, target_class)
+        if not os.path.isdir(target_dir):
+            continue
+        for instance_dir in os.scandir(target_dir):
+            if not instance_dir.is_dir():
+                continue
+            paths = []
+            for root, _, fnames in sorted(os.walk(instance_dir.path, followlinks=True)):
+                for fname in sorted(fnames):
+                    path = os.path.join(root, fname)
+                    if is_valid_file(path):
+                        paths.append(path)
+            if len(paths) > 0:
+                item = paths, class_index
+                instances.append(item)
+    return instances
+
+
+class DatasetFolder(VisionDataset):
+
+    def __init__(self, root, loader, extensions=None, transform=None,
+                 target_transform=None, is_valid_file=None):
+        super(DatasetFolder, self).__init__(root, transform=transform,
+                                            target_transform=target_transform)
+        classes, class_to_idx = self._find_classes(self.root)
+        samples = make_dataset(self.root, class_to_idx, extensions, is_valid_file)
+        if len(samples) == 0:
+            msg = "Found 0 files in subfolders of: {}\n".format(self.root)
+            if extensions is not None:
+                msg += "Supported extensions are: {}".format(",".join(extensions))
+            raise RuntimeError(msg)
+
+        self.loader = loader
+        self.extensions = extensions
+
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+
+    def _find_classes(self, dir):
+        classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+    def __getitem__(self, index):
+        paths, target = self.samples[index]
+        samples = None
+        for path in paths:
+            sample = self.loader(path)
+            if self.transform is not None:
+                sample = self.transform(sample)
+            if not torch.is_tensor(sample):
+                raise RuntimeError("Sample is not torch.Tensor")
+            if samples is None:
+                samples = sample.unsqueeze(0)
+            else:
+                samples = torch.cat((samples, sample.unsqueeze(0)), dim=0)
+            
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return samples, target
+
+    def __len__(self):
+        return len(self.samples)
+
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+
+class ImageFolder(DatasetFolder):
+
+    def __init__(self, root, transform=None, target_transform=None,
+                 loader=default_loader, is_valid_file=None):
+        super(ImageFolder, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                                          transform=transform,
+                                          target_transform=target_transform,
+                                          is_valid_file=is_valid_file)
+        self.imgs = self.samples
