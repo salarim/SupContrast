fd5deca76daabccd4aefddcfe115dad2bdd3d8a8
Mohammad Hadi Salari <salari.m1375@gmail.com>
2020-08-09 17:11:54 -0700

    Add meta_job that set environment variables of job script

--- job.sh
+++ job.sh
@@ -2,13 +2,18 @@
 #SBATCH --mail-user=salari.m1375@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-mori_gpu
-#SBATCH --job-name=SimCLR-shapenet-bs2048-v2-dr0.4-e100-resnet18
+#SBATCH --job-name=SimCLR-shapenet-bs2048-v2-dr0.0-e100-resnet18
 #SBATCH --output=%x-%j.out
 #SBATCH --ntasks=1
 #SBATCH --time=2:59:00
 #SBATCH --mem=10000M
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --cpus-per-task=16
+
+export BATCH_SIZE="${BATCH_SIZE:=2048}"
+export VIEWS="${VIEWS:=2}"
+export DROP_RATIO="${DROP_RATOI:=0.0}"
+
 
 cd $SLURM_TMPDIR
 cp -r ~/scratch/SupContrast .
@@ -30,8 +35,9 @@ pip install --no-index -r requirements.txt
 #python main_ce.py --epochs 100 --batch_size 128 --learning_rate 0.8 --cosine --model resnet18 --dataset shapenet \
 #	--data-folder datasets/shapenet --views 2
 
-python main_supcon.py --epochs 100 --batch_size 2048 --learning_rate 0.5 --temp 0.1 --cosine --model resnet18 --dataset shapenet \
-	--data-folder datasets/shapenet --views 2 --drop-objects-ratio 0.4 --method SimCLR
+
+python main_supcon.py --epochs 100 --batch_size $BATCH_SIZE --learning_rate 0.5 --temp 0.1 --cosine --model resnet18 --dataset shapenet \
+	--data-folder datasets/shapenet --views $VIEWS --drop-objects-ratio $DROP_RATIO --method SimCLR
 
 python main_linear.py --epochs 20 --batch_size 128 --learning_rate 5 --model resnet18 --ckpt ./save/SupCon/shapenet_models/*/last.pth \
 	--dataset shapenet --data-folder datasets/shapenet --views 30
---
+++ meta_job.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+from itertools import product
+
+env_vars = [{'BATCH_SIZE':['2048','1024','512','256','128'],
+             'VIEWS':['2','4','8','16','30'],
+             'DROP_RATIO':['0.0','0.5','0.75','0.875','0.933']}]
+
+independent_sets = []
+for independent_dict in env_vars:
+    independent_set = []
+    for values in zip(*independent_dict.values()):
+        d = {}
+        for i, k in enumerate(independent_dict.keys()):
+            d[k] = values[i]
+        independent_set.append(d)
+    independent_sets.append(independent_set)
+
+for env_dicts in product(*independent_sets):
+    for env_dict in env_dicts:
+        for k, v in env_dict.items():
+            os.environ[k] = v
+    job_name = "SimCLR-shapenet-bs{}-v{}-dr{}-e100-resnet18".format(os.environ['BATCH_SIZE'],
+                                                                    os.environ['VIEWS'],
+                                                                    os.environ['DROP_RATIO'])
+
+    rc = subprocess.check_call(["sbatch",
+                                "--job-name={}".format(job_name),
+                                "job.sh"])
+
